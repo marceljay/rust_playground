799a2c9f9ed1ca1645afa985f502998ce688f0fe
Marcel Jackisch <jackisch@protonmail.com>
2021-04-27 21:12:22 +0200

    starting macros

--- src/macros.rs
+++ src/macros.rs
@@ -1,8 +1,10 @@
 // Macros
+// Metaprogramming: macros are a way of writing code that writes other code
 // can be used for variadic functions (like println!())
 // rust is an expression-based language
 
-// General Syntax for custom macros
+
+// General Syntax for "declarative macros" created via keyword "macro_rules!"
 macro_rules! macro_name {
     () => {
         // What it does, e.g
@@ -11,22 +13,43 @@ macro_rules! macro_name {
 }
 
 #[allow(unused_macros)] 
+#[macro_export] // makes it available whenever the crate is brought into scope
 macro_rules! expr_macro {
-        
+        // Syntax similar to a Rust match expression, this macro has multiple arms
+        // macro_rules!-Macros compare a value (literal) to patterns associated with code
+        // <pattern> => <associated code to be emitted in case of match>
         (x => $e:expr) => {println!("I can take expressions as argument {}", $e);};
-        (y => $e:expr) => {println!("I can take expressions as argument {}", $e);};
+        (y => $e2:  expr) => {println!("I can take expressions as argument {}", $e2);};
+        (token q(-_-)p $e:expr) => {println!("first part can be anything {:?}", $e);};
+        ( $e:expr) => {println!("I can take expressions as argument {}", $e);};
 
+        ($st:stmt) => {$st};
+
+
+
+        // $e:expr matches any expression, $e is the expression's name
 }
 
+// Procedural Macros (sort of like a mapping on input code)
+// All procedural macros work in a similar fashion.
+// The types are: custom derive, attribute-like, and function-like
+// Example: 
+// #[some_attribute]
 
 #[allow(unused_variables)] 
 pub fn run() {
     println!("\nmodule macros: run() called \n---------");
     macro_name!();
-    expr_macro!(x => 1 + 1);
-    expr_macro!(y => if 0 < 1 {true} else {false});
+    expr_macro![x => 1 + 1]; // Note that the delimiter can be different
+    expr_macro!{x => 1 + 1}; 
+
+    expr_macro!(y => if 0 < 1 {let b = true; b} else {let b = false; b});
 
     // ERROR: no rules for token z
     // expr_macro!(z => 1 + 1);
+    expr_macro!(token q(-_-)p false);
+    expr_macro!(let numba: u32 = 5);
+
+    
 
 }
